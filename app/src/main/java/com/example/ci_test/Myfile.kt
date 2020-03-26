2f1abf940e1f200e6cef1f7e214d045cf7cbe514
Wanaka <jonas.haag@hotmail.com>
2020-03-26 14:50:17 +0100

    updated test

--- app/src/main/java/com/example/ci_test/Myfile.kt
+++ app/src/main/java/com/example/ci_test/Myfile.kt
@@ -2,7 +2,7 @@ package com.example.ci_test
 
 class Myfile {
 
-//    fun sum (a: Int, b: Int): Int{
-//        return a + b
-//    }
+    fun sum (a: Int, b: Int): Int{
+        return a + b
+    }
 }
